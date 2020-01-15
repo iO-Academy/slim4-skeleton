87d50b94c9ff9efe98f05688ca4bd139ecd93145
Ashley Coles <ashley@acoles.co.uk>
2020-01-15 09:35:51 +0000

    Update index template for slim 4

--- templates/index.php
+++ templates/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Slim 3</title>
+    <title>Slim 4</title>
     <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
     <style>
         body {
