cbf48d671352bb3c9d25a464643556907b6e95e3
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2020-05-13 17:59:24 +0100

    changed the default view example

--- app/routes.php
+++ app/routes.php
@@ -9,7 +9,13 @@ return function (App $app) {
 
     $app->get('/', function ($request, $response, $args) use ($container) {
         $renderer = $container->get('renderer');
-        return $renderer->render($response, "index.php", $args);
+        return $renderer->render($response, "homepage.phtml");
+    });
+
+
+    $app->get('/{name}', function ($request, $response, $args) use ($container) {
+        $renderer = $container->get('renderer');
+        return $renderer->render($response, "homepage.phtml", $args);
     });
 
 };
