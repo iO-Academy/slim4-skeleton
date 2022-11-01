58d8d63f9f05132ce95d8c57dce7d2b4d88c00ad
Joshua Bennet <josh@thebennets.net>
2022-11-01 14:24:40 +0000

    first commit to bookCollection

--- templates/index.php
+++ templates/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Slim 4</title>
+    <title>Book Collection</title>
     <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
     <style>
         body {
