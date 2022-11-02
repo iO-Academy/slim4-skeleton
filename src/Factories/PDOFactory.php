c6dd50b005a174e6bfa005415a522767da4f110a
Joshua Bennet <josh@thebennets.net>
2022-11-02 11:18:52 +0000

    setting up the PDOFactory

---
+++ src/Factories/PDOFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Factories;
+
+use PDO;
+use Psr\Container\ContainerInterface;
+
+class PDOFactory
+{
+    public function __invoke(ContainerInterface $container): PDO
+    {
+        $db = new PDO('mysql:host127.0.0.1;dbname=bookCollection', 'root', 'password');
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $db;
+    }
+}
+
